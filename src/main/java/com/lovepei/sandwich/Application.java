939cc04f12744d78555b74ea483afd9fc57a1607
unknown <>
2017-01-08 12:09:15 +0800

    springBoot 整合 mybaties,加入logback配置

--- src/main/java/com/lovepei/sandwich/Application.java
+++ src/main/java/com/lovepei/sandwich/Application.java
@@ -7,7 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 /**
  * Created by cumt_cx on 2017/1/6.
  */
-@RestController
 @SpringBootApplication
 public class Application {
 
--- src/main/java/com/lovepei/sandwich/web/TestController.java
+++ src/main/java/com/lovepei/sandwich/web/TestController.java
@@ -1,12 +1,25 @@
 package com.lovepei.sandwich.web;
 
+import com.lovepei.sandwich.domain.User;
+import com.lovepei.sandwich.service.UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
 
 /**
  * Created by cumt_cx on 2017/1/6.
  */
+@RestController
 public class TestController {
+
+    private final  static Logger logger = LoggerFactory.getLogger(TestController.class);
+
+    @Autowired
+    private UserService userService;
 
     @ResponseBody
     @RequestMapping(value = "/")
@@ -16,7 +29,9 @@ public class TestController {
 
     @ResponseBody
     @RequestMapping(value="/getUser")
-    public String getUser(){
-        return "";
+    public User getUser(){
+        User user = userService.getUser("admin");
+        logger.debug(user.toString());
+        return user;
     }
 }
